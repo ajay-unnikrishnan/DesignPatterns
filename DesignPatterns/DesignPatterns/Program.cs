a28f458072195c75e7ae732083ee8c4dbd6b8484
Unnikrishnan, Ajay <Ajay.Unnikrishnan@boeing.com>
2023-04-22 16:40:43 +0530

    Singleton

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            new ValidateSingleton().Validate();
+        }
+        
+
+    }
+}
