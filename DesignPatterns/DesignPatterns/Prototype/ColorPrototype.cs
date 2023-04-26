32ee95d9a0892a0b7e6c4e19afd8e16974195540
Unnikrishnan, Ajay <Ajay.Unnikrishnan@boeing.com>
2023-04-26 12:40:25 +0530

    prototype pattern

---
+++ DesignPatterns/DesignPatterns/Prototype/ColorPrototype.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Prototype
+{
+    public class ColorPrototype
+    {
+        public void Debug()
+        {
+            ColorManager colorManager = new ColorManager();
+            colorManager["red"] = new Color(255, 0, 0);
+            colorManager["green"] = new Color(0, 255, 0);
+            colorManager["blue"] = new Color(0, 0, 255);
+
+            colorManager["angry"] = new Color(255, 54, 0);
+
+            Color color1 = colorManager["red"].Clone() as Color;
+            var color2 = colorManager["angry"].Clone() as Color;
+            Console.ReadKey();
+
+        }
+    }
+    public interface IColorProtoType
+    {
+        IColorProtoType Clone();
+    }
+    public class Color : IColorProtoType
+    {
+        int red, green, blue;
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+        public IColorProtoType Clone()
+        {
+            return this.MemberwiseClone() as IColorProtoType;
+        }
+        public int x()
+        {
+            return 1;
+        }
+    }
+    public class ColorManager
+    {
+        private Dictionary<string, IColorProtoType> colors = new Dictionary<string, IColorProtoType>();
+        public IColorProtoType this[string key]
+        {
+            get { return colors[key]; }
+            set { colors.Add(key, value); }
+        }
+
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Prototype/EmployeeProtoType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DesignPatterns.Prototype
+{
+    public class EmployeeProtoType
+    {
+        public void Debug()
+        {
+            var _employee = new Employee();
+            _employee.Name = "Ajay";
+            _employee.Role = "Developer";
+            _employee.Department = "P Systems";
+
+
+            var _employeeCopy = (Employee)_employee.Clone();
+            _employeeCopy.Name = "X"; // All the values are copied from the 1st object
+            Console.ReadKey();
+
+
+        }
+    }
+    public interface IPrototype
+    {
+        IPrototype Clone();
+    }
+    public class Employee : IPrototype
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Department { get; set; }
+
+        public IPrototype Clone()
+        {
+            /*Shallow Copy: only top-level objects are duplicated
+            ie. if the object contains any reference types then only the address
+            of that reference type will be copied from source to target*/
+            return (IPrototype)MemberwiseClone();
+        }
+    }
+}
