2a175042e2161b254cf815c0049160c22575ffa7
Unnikrishnan, Ajay <Ajay.Unnikrishnan@boeing.com>
2023-05-15 14:37:52 +0530

    Car Builder

--- DesignPatterns/DesignPatterns/Builder/CarBuilder.cs
+++ DesignPatterns/DesignPatterns/Builder/CarBuilder.cs
@@ -2,9 +2,15 @@
 
 namespace DesignPatterns.Builder
 {
+    /// <summary>
+    /// The Director controls the construction process using a builder object.
+    /// </summary>
     public class Director
     {
-        public void Debug()
+        /// <summary>
+        /// Builds a car using a specific builder.
+        /// </summary>
+        public void BuildCar()
         {
             var builder = new CarBuilder();
             Car obj = builder.SetColor("Red")
@@ -13,35 +19,15 @@ namespace DesignPatterns.Builder
                 .Build();
         }
     }
-
-    public class CarBuilder : ICarBuilder
-    {
-        Car _car = new Car();
-        public ICarBuilder SetColor(string color)
-        {
-            _car.Color = color;
-            return this;
-        }
-
-        public ICarBuilder SetMake(string make)
-        {
-            _car.Make = make;
-            return this;
-        }
-
-        public ICarBuilder SetManufactureDate(DateTime date)
-        {
-            _car.ManuifactureDate = date.ToString();
-            return this;
-        }
-        public Car Build() => _car;
-    }
     public class Car
     {
         public string Make { get; set; }
         public string Color { get; set; }
         public string ManuifactureDate { get; set; }
     }
+    /// <summary>
+    /// Defines the interface for building the product
+    /// </summary>
     public interface ICarBuilder
     {
         ICarBuilder SetMake(string make);
@@ -49,4 +35,41 @@ namespace DesignPatterns.Builder
         ICarBuilder SetManufactureDate(DateTime date);
         Car Build();
     }
+    /// <summary>
+    /// The CarBuilder  is responsible for constructing and assembling the parts of the Car object.
+    /// </summary>
+    public class CarBuilder : ICarBuilder
+    {
+        Car _car = new Car();
+
+        /// <summary>
+        /// Sets the color of the car.
+        /// </summary>
+        public ICarBuilder SetColor(string color)
+        {
+            _car.Color = color;
+            return this;
+        }
+        /// <summary>
+        /// Sets the make of the car.
+        /// </summary>
+        public ICarBuilder SetMake(string make)
+        {
+            _car.Make = make;
+            return this;
+        }
+        /// <summary>
+        /// Sets the manufacture date of the car.
+        /// </summary>
+        public ICarBuilder SetManufactureDate(DateTime date)
+        {
+            _car.ManuifactureDate = date.ToString();
+            return this;
+        }
+        /// <summary>
+        /// Builds and returns the car object.
+        /// </summary>
+        public Car Build() => _car;
+    }
+    
 }
